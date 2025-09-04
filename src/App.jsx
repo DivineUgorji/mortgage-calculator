b99f1e195020205fb293364e75814b6e92a69300
Divine Ugorji <divineugorji37@gmail.com>
2025-09-04 10:55:32 +0100

    Implement custom fonts 🎉

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
+    <div className="font-jakarta">
       {/* <h1 className="text-green-700 font-bold text-5xl font-jakarta">
         Mortgage calculator
       </h1> */}
